0ca48285a6c09876a119360af7bf01bcb9d1d063
forneiro <nforneirorosas@gmail.com>
2024-01-14 22:33:23 -0300

    interface modified

--- JS/scripts.js
+++ JS/scripts.js
@@ -32,3 +32,27 @@ sound.play();
 sound.fade(0, 1, 1000);  //Aumenta el volumen de 0 a 1 en 1 segundo (1000 milisegundos)
 
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("scroll", function () {
+    var nav = document.querySelector(".nav");
+    nav.style.transform = "translateY(" + window.scrollY + "px)";
+  });
+
+  var smoothScrollLinks = document.querySelectorAll('.smooth-scroll');
+  smoothScrollLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      var targetId = link.getAttribute('href').substring(1);
+      var targetElement = document.getElementById(targetId);
+
+      // Realiza el desplazamiento suave
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+});
+
